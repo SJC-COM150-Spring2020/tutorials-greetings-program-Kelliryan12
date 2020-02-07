23101209d3fd7b098bb29390c7ac7ae443505e09
Kelliryan12 <60245183+Kelliryan12@users.noreply.github.com>
2020-02-06 19:13:30 -0500

    Print Greetings to Messagebox
    Print greetings message using messagebox

---
+++ Greetings3/Greetings3/Greetings3.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Greetings3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnClick = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnClick
+        '
+        Me.btnClick.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.btnClick.Font = New System.Drawing.Font("Book Antiqua", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClick.ForeColor = System.Drawing.Color.White
+        Me.btnClick.Location = New System.Drawing.Point(17, 284)
+        Me.btnClick.Name = "btnClick"
+        Me.btnClick.Size = New System.Drawing.Size(173, 42)
+        Me.btnClick.TabIndex = 0
+        Me.btnClick.Text = "Click Me!"
+        Me.btnClick.UseVisualStyleBackColor = False
+        '
+        'txtName
+        '
+        Me.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtName.Font = New System.Drawing.Font("Book Antiqua", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(17, 230)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(191, 33)
+        Me.txtName.TabIndex = 1
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Book Antiqua", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(12, 28)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(196, 26)
+        Me.lblHeading.TabIndex = 2
+        Me.lblHeading.Text = "Greetings Program"
+        '
+        'Greetings3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(547, 498)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnClick)
+        Me.Name = "Greetings3"
+        Me.Text = "Greetings3"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnClick As Button
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents lblHeading As Label
+End Class
---
+++ Greetings3/Greetings3/Greetings3.vb
@@ -0,0 +1,9 @@
+﻿Public Class Greetings3
+
+
+    Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
+        'Take value from name text box and print in messagebox
+        MessageBox.Show(txtName.Text)
+
+    End Sub
+End Class
